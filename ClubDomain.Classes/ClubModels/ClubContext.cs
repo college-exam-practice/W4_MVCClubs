cb3cef4cdc95c92a269ca989333c69a49c6b963c
s00083446 <s00083446@mail.itsligo.ie>
2019-07-21 20:52:06 +0100

    DbContext added to ClubContext file and EF6 package added

---
+++ ClubDomain.Classes/ClubModels/ClubContext.cs
@@ -0,0 +1,26 @@
+﻿using ClubDomain.Classes.ClubModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clubs.Model
+{
+    public class ClubContext : DbContext
+    {
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<ClubEvent> ClubEvents { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<EventAttendnace> EventAttendees { get; set; }
+        public ClubContext():base("ClubsConnection")
+        {
+
+        }
+        public static ClubContext Create()
+        {
+            return new ClubContext();
+        }
+    }
+}
