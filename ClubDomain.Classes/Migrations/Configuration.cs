0b2883b2ba79beff84fa2adf97de8a6424da6486
s00083446 <s00083446@mail.itsligo.ie>
2019-08-21 06:51:36 +0100

    added course and student tables and data to import from csv file

--- ClubDomain.Classes/Migrations/Configuration.cs
+++ ClubDomain.Classes/Migrations/Configuration.cs
@@ -5,6 +5,12 @@ namespace ClubDomain.Classes.Migrations
     using System.Collections.Generic;
     using System.Data.Entity.Migrations;
     using System.Globalization;
+    using System.IO;
+    using System.Reflection;
+    using System.Text;
+    using System.Linq;
+    using CsvHelper;
+    using System.Diagnostics;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Clubs.Model.ClubContext>
     {
@@ -15,7 +21,33 @@ namespace ClubDomain.Classes.Migrations
 
         protected override void Seed(Clubs.Model.ClubContext context)
         {
-          
+            List<Course> courses = new List<Course>();
+            // Get the current assembly
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            // Assembly name and resource stored in assembly
+            string resourceName = "Week4.Console.Courses.csv";
+            // Get the embedded resource from the assembly
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {   // create a stream reader
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    // create a csv reader dor the stream
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.HasHeaderRecord = false;
+                    courses = csvReader.GetRecords<Course>().ToList();
+                    context.Courses.AddOrUpdate(c => c.CourseCode);
+                    //context.Courses.AddOrUpdate(c => c.YearOfStudy);
+                    //context.Courses.AddOrUpdate(c => c.CouresTitle);
+                    // Read the records into the desired collection of that type
+                    // and iterate over the collection
+                    foreach (var item in courses)
+                    {
+                        context.Courses.AddOrUpdate();
+                    }
+                    System.Console.ReadKey();
+                }
+            }
+
         }
     }
 }
